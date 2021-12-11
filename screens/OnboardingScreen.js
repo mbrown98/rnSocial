13980b6f119174af852424dbd52d30f538976cae
Matthew Brown <mbrown@geomarvel.com>
2021-12-11 18:49:34 -0500

    onboading UI implemenation

--- screens/OnboardingScreen.js
+++ screens/OnboardingScreen.js
@@ -1,21 +1,40 @@
-import React from 'react'
-import { View, Text, Button, StyleSheet } from 'react-native'
+import React from 'react';
+import {View, Text, Button, StyleSheet, Image} from 'react-native';
+import Onboarding from 'react-native-onboarding-swiper';
 
-const OnboardingScreen = ({ navigation }) => {
-    return (
-        <View style={styles.container}>
-            <Text>OnboardingScreen</Text>
-            <Button title="Click Here" onPress={() => navigation.navigate("Login")} />
-        </View>
-    )
-}
+const OnboardingScreen = ({navigation}) => {
+  return (
+    <Onboarding
+      pages={[
+        {
+          backgroundColor: '#a6e4d0',
+          image: <Image source={require('../assets/onboarding-img1.png')} />,
+          title: 'Connect to the World',
+          subtitle: 'A New Way To Connect With The World',
+        },
+        {
+          backgroundColor: '#fdeb93',
+          image: <Image source={require('../assets/onboarding-img2.png')} />,
+          title: 'Share Your Favorites',
+          subtitle: 'Share Your Thoughts With Similar People',
+        },
+        {
+          backgroundColor: '#e9bcbe',
+          image: <Image source={require('../assets/onboarding-img3.png')} />,
+          title: 'Become The Star',
+          subtitle: 'Let the Spotlight Capture You',
+        },
+      ]}
+    />
+  );
+};
 
 export default OnboardingScreen;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-    }
-})
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});

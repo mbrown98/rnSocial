8ddeefcccfc21dbb9d6048a18eda9360e058a581
Matthew Brown <mbrown@geomarvel.com>
2021-12-11 20:15:40 -0500

    icon package added

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,21 +1,26 @@
-import React from 'react'
-import { View, Text, Button, StyleSheet } from 'react-native'
+import React from 'react';
+import {View, Text, Button, StyleSheet} from 'react-native';
+import {IconFill, IconOutline} from '@ant-design/icons-react-native';
 
- const LoginScreen = ({ navigation }) => {
-    return (
-        <View style={styles.container}>
-            <Text>LoginScreen</Text>
-            <Button title="Click Here" onPress={() => navigation.navigate("Button Clicked")} />
-        </View>
-    )
-}
+const LoginScreen = ({navigation}) => {
+  return (
+    <View style={styles.container}>
+      <IconOutline name="user" size={25} color="#666" />
+      <Text>Login Screen</Text>
+      <Button
+        title="Click Here"
+        onPress={() => navigation.navigate('Signup')}
+      />
+    </View>
+  );
+};
 
 export default LoginScreen;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-    }
-})
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});

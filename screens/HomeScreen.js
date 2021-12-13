19d1ef9e747d7a72debd9ae1f71cee573414f1a6
Matthew Brown <mbrown@geomarvel.com>
2021-12-12 22:08:59 -0500

    added homescreen

---
+++ screens/HomeScreen.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import FormButton from '../components/FormButton';
+
+const HomeScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Welcome</Text>
+      <FormButton buttonTitle="Logout" onPress={() => {}} />
+    </View>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#f9fafd',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  text: {
+    fontSize: 30,
+    color: '#333333',
+  },
+});
